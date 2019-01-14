16de66ca7433e91b4243a3027b47399599e0580c
Wei Sun <sw@gfloan.com>
2019-01-14 19:21:54 +0800

    upload to bugly

--- fastlane/Fastfile.rb
+++ fastlane/Fastfile.rb
@@ -15,7 +15,7 @@
 
 default_platform(:ios)
 
-def build_and_upload_to_testflight_with_scheme(scheme)
+def build_and_upload_to_testflight_with_scheme(scheme, bugly_app_key, bugly_app_id)
   app_identifier = CredentialsManager::AppfileConfig.try_fetch_value(:app_identifier)
   # ensure_git_status_clean
   disable_automatic_code_signing
@@ -33,17 +33,31 @@ def build_and_upload_to_testflight_with_scheme(scheme)
       :PROVISIONING_PROFILE_SPECIFIER => "match Development #{app_identifier}"
   }
 
+  ipa_name = "#{scheme}.ipa"
   build_app(
       scheme: scheme,
       xcargs: settings_to_override,
       export_method: "app-store",
+      output_name: ipa_name,
       export_options: {
           provisioningProfiles: {
               app_identifier => "match AppStore #{app_identifier}"
           }
       }
   )
+
+  upload_app_to_bugly(
+      file_path: ipa_name,
+      app_key: bugly_app_key,
+      app_id: bugly_app_id,
+      pid:"2",
+      title:"title",
+      desc:"description",
+      )
+
   upload_to_testflight(skip_waiting_for_build_processing:true )
+
+
 end
 
 platform :ios do
@@ -52,13 +66,19 @@ platform :ios do
 
     scheme = "LifeInGuangZhou"
 
-    build_and_upload_to_testflight_with_scheme(scheme)
+    bugly_app_key = "52d1630f-be0a-4cab-8145-e4662b87446e"
+    bugly_app_id = "6e66df30b1"
+
+    build_and_upload_to_testflight_with_scheme(scheme, bugly_app_key, bugly_app_id)
 
   end
+
   lane :drone_test_beta do
     scheme = "LifeInGuangZhou"
+    bugly_app_key = "52d1630f-be0a-4cab-8145-e4662b87446e"
+    bugly_app_id = "6e66df30b1"
 
-    build_and_upload_to_testflight_with_scheme(scheme)
+    build_and_upload_to_testflight_with_scheme(scheme, bugly_app_key, bugly_app_id)
   end
 
   lane :online_beta do
